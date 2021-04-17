4113e9cabb198d36283513a137b170229d6c2c18
Jesus A Rocha V <jesus_reddead@hotmail.com>
2021-04-16 20:56:51 -0500

    #8 Controlador Article

--- routes/api.php
+++ routes/api.php
@@ -19,23 +19,8 @@ use App\Models\Article;
 //     return $request->user();
 // });
 
-Route::get('articles', function(){
-    return Article::all();
-});
-Route::get('articles/{id}', function($id){
-    return Article::find($id);
-});
-
-Route::post('articles', function(Request $request){
-    return Article::create($request->all());
-});
-
-Route::put('articles/{id}', function(Request $request, $id){
-    $article = Article::findOrFail($id);
-    $article->update($request->all());return $article;
-});
-
-Route::delete('articles/{id}', function($id){
-    Article::find($id)->delete();
-    return 204;
-});
+Route::get('articles', [ArticleController::class,'index']);
+Route::get('articles/{article}', [ArticleController::class,'show']);
+Route::post('articles', [ArticleController::class,'store']);
+Route::put('articles/{article}', [ArticleController::class,'update']);
+Route::delete('articles/{article}', [ArticleController::class,'delete']);
