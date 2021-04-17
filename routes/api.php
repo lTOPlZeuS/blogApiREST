9a04d18002ae9d47d510facdc779f26dc248f4a3
Jesus A Rocha V <jesus_reddead@hotmail.com>
2021-04-17 12:32:42 -0500

    #9 Auth JWT

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Article;
-
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,12 +15,14 @@ use App\Models\Article;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
+Route::post('register', [UserController::class,'register']);
+Route::post('login', [UserController::class,'authenticate']);
 Route::get('articles', [ArticleController::class,'index']);
-Route::get('articles/{article}', [ArticleController::class,'show']);
-Route::post('articles', [ArticleController::class,'store']);
-Route::put('articles/{article}', [ArticleController::class,'update']);
-Route::delete('articles/{article}', [ArticleController::class,'delete']);
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('user', [UserController::class,'getAuthenticatedUser']);
+    Route::get('articles/{article}', [ArticleController::class,'show']);
+    Route::post('articles', [ArticleController::class,'store']);
+    Route::put('articles/{article}', [ArticleController::class,'update']);
+    Route::delete('articles/{article}', [ArticleController::class,'delete']);
+});
