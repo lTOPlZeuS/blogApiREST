22da57fae79c42707ba449ac38251602226482e9
Jesus A Rocha V <jesus_reddead@hotmail.com>
2021-04-19 14:12:32 -0500

    #19 validation article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,22 +9,31 @@ use App\Models\Article;
 
 class ArticleController extends Controller
 {
-public function index(){
-    return new ArticleCollection(Article::paginate(10));
-}
-public function show(Article $article){
-    return response()->json(new ArticleResource($article),200);
-}
-public function store(Request $request){
-    $article = Article::create($request->all());
-    return response()->json($article, 201);
-}
-public function update(Request $request, Article $article){
-    $article->update($request->all());
-    return response()->json($article, 200);
-}
-public function delete(Article $article){
-    $article->delete();
-    return response()->json(null, 204);
-}
+    private static $rule = [
+            'title' => 'required|string|unique:articles|max:255',
+            'body' => 'required',
+    ];
+    private static $messages=[
+            'required'=>'El campo :attribute es obligatorio.',
+            'unique'=> 'El campo title es unico',
+    ];
+    public function index(){
+        return new ArticleCollection(Article::paginate(10));
+    }
+    public function show(Article $article){
+        return response()->json(new ArticleResource($article),200);
+    }
+    public function store(Request $request){
+        $request->validate(self::$rule,self::$messages);
+        $article = Article::create($request->all());
+        return response()->json($article, 201);
+    }
+    public function update(Request $request, Article $article){
+        $article->update($request->all());
+        return response()->json($article, 200);
+    }
+    public function delete(Article $article){
+        $article->delete();
+        return response()->json(null, 204);
+    }
 }
